7c9189d0a9af15b754d2efdb0f7451af2c653cc1
Mariela Gonzales <mpgonzales14@gmail.com>
2019-03-03 20:13:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function destructivelyRemoveFirstKitten(name) {
   return kittens
 }
 
-function appendKitten(name){
+function appendKitten(name) {
   return [...kittens, name]
 }
 
@@ -32,9 +32,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1);
+  return kittens.slice(0, kittens.length - 1)
 }
 
 function removeFirstKitten() {
-  return kittens.slice(1);
+  return kittens.slice(1)
 }
